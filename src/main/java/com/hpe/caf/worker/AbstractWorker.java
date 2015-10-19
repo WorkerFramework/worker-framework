9f12fbfa82244d738d6614c504b6ddf34581c0a9
Richard D.G. Hickman <richard.hickman@hp.com>
2015-10-19 15:35:14 +0100

    Initial commit for worker-caf

---
+++ src/main/java/com/hpe/caf/worker/AbstractWorker.java
@@ -0,0 +1,207 @@
+package com.hpe.caf.worker;
+
+
+import com.hpe.caf.api.Codec;
+import com.hpe.caf.api.CodecException;
+import com.hpe.caf.api.worker.InvalidTaskException;
+import com.hpe.caf.api.worker.TaskFailedException;
+import com.hpe.caf.api.worker.TaskStatus;
+import com.hpe.caf.api.worker.Worker;
+import com.hpe.caf.api.worker.WorkerResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.util.Objects;
+import java.util.Set;
+
+
+/**
+ * A partial Worker implementation with utility methods.
+ *
+ * @param <T> the task class for this Worker
+ * @param <V> the result class for this Worker
+ * @since 9.0
+ */
+public abstract class AbstractWorker<T,V> implements Worker
+{
+    private final T task;
+    private final String resultQueue;
+    private final Codec codec;
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractWorker.class);
+
+
+    /**
+     * Create a Worker. The input task will be validated.
+     * @param task the input task for this Worker to operate on
+     * @param resultQueue the reference to the queue that should take results from this type of Worker
+     * @param codec used to serialising result data
+     * @throws InvalidTaskException if the input task does not validate successfully
+     */
+    public AbstractWorker(final T task, final String resultQueue, final Codec codec)
+        throws InvalidTaskException
+    {
+        this.task = Objects.requireNonNull(task);
+        this.resultQueue = Objects.requireNonNull(resultQueue);
+        this.codec = Objects.requireNonNull(codec);
+        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        Set<ConstraintViolation<T>> violations = validator.validate(task);
+        if ( violations.size() > 0 ) {
+            LOG.error("Task of type {} failed validation due to: {}", task.getClass().getSimpleName(), violations);
+            throw new InvalidTaskException("Task failed validation");
+        }
+    }
+
+
+
+    @Override
+    public final WorkerResponse getGeneralFailureResult(final Throwable t)
+    {
+        return new WorkerResponse(getResultQueue(), TaskStatus.RESULT_EXCEPTION, getExceptionData(t), getWorkerIdentifier(), getWorkerApiVersion(), null);
+    }
+
+
+    /**
+     * @return the task for this Worker to operate on
+     */
+    protected final T getTask()
+    {
+        return this.task;
+    }
+
+
+    /**
+     * @return the reference to the queue that should contain the results from this type of Worker
+     */
+    protected final String getResultQueue()
+    {
+        return this.resultQueue;
+    }
+
+
+    /**
+     * @return the Codec supplied to the Worker from the framework
+     */
+    protected final Codec getCodec()
+    {
+        return this.codec;
+    }
+
+
+    /**
+     * Utility method for creating a WorkerReponse that represents a successful result.
+     * @param result the result from the Worker
+     * @return a WorkerResponse that represents a successful result containing the specified task-specific serialised message
+     */
+    protected final WorkerResponse createSuccessResult(final V result)
+    {
+        return createSuccessResult(result, null);
+    }
+
+
+    /**
+     * Utility method for creating a WorkerReponse that represents a successful result with context data.
+     * @param result the result from the Worker
+     * @param context the context entries to add to the published message
+     * @return a WorkerResponse that represents a successful result containing the specified task-specific serialised message
+     */
+    protected final WorkerResponse createSuccessResult(final V result, final byte[] context)
+    {
+        try {
+            byte[] data = ( result != null ? getCodec().serialise(result) : new byte[]{} );
+            return new WorkerResponse(getResultQueue(), TaskStatus.RESULT_SUCCESS, data, getWorkerIdentifier(), getWorkerApiVersion(), context);
+        } catch (CodecException e) {
+            throw new TaskFailedException("Failed to serialise result", e);
+        }
+    }
+
+
+    /**
+     * Utility method for creating a WorkerReponse that represents a failed result.
+     * @param result the result from the Worker
+     * @return a WorkerResponse that represents a failed result containing the specified task-specific serialised message
+     */
+    protected final WorkerResponse createFailureResult(final V result)
+    {
+        return createFailureResult(result, null);
+    }
+
+
+    /**
+     * Utility method for creating a WorkerReponse that represents a failed result with context data.
+     * @param result the result from the Worker
+     * @param context the context entries to add to the published message
+     * @return a WorkerResponse that represents a failed result containing the specified task-specific serialised message
+     */
+    protected final WorkerResponse createFailureResult(final V result, final byte[] context)
+    {
+        try {
+            byte[] data = ( result != null ? getCodec().serialise(result) : new byte[]{} );
+            return new WorkerResponse(getResultQueue(), TaskStatus.RESULT_FAILURE, data, getWorkerIdentifier(), getWorkerApiVersion(), context);
+        } catch (CodecException e) {
+            throw new TaskFailedException("Failed to serialise result", e);
+        }
+    }
+
+
+    /**
+     * Utility method for creating a new task submission to an arbitrary queue. This is for Workers that are chaining jobs
+     * to other Workers.
+     * @param queue the reference of the queue to put the message on
+     * @param data the serialised task-specific message for the Worker to perform the work
+     * @param messageIdentifier the classifier for the task-specific message
+     * @param messageApiVersion the API version for the task-specific message
+     * @return a WorkerResponse that represents a new task submission to a specific queue
+     */
+    protected final WorkerResponse createTaskSubmission(final String queue, final byte[] data, final String messageIdentifier, final int messageApiVersion)
+    {
+        return createTaskSubmission(queue, data, messageIdentifier, messageApiVersion, null);
+    }
+
+
+    /**
+     * Utility method for creating a new task submission to an arbitrary queue with context data. This is for Workers that are
+     * chaining jobs to other Workers.
+     * @param queue the reference of the queue to put the message on
+     * @param data the serialised task-specific message for the Worker to perform the work
+     * @param messageIdentifier the classifier for the task-specific message
+     * @param messageApiVersion the API version for the task-specific message
+     * @param context the context entries to add to the published message
+     * @return a WorkerResponse that represents a new task submission to a specific queue
+     */
+    protected final WorkerResponse createTaskSubmission(final String queue, final byte[] data, final String messageIdentifier, final int messageApiVersion,
+                                                        final byte[] context)
+    {
+        return new WorkerResponse(queue, TaskStatus.NEW_TASK, data, messageIdentifier, messageApiVersion, context);
+    }
+
+
+    /**
+     * Utility method to check the interrupted flag of the current Thread and throw InterruptedException if true.
+     * @throws InterruptedException if the current Thread is interrupted
+     */
+    protected final void checkIfInterrupted()
+        throws InterruptedException
+    {
+        if ( Thread.currentThread().isInterrupted() ) {
+            throw new InterruptedException("Worker interrupted");
+        }
+    }
+
+
+    /**
+     * @param t the Throwable from the Worker
+     * @return a byte array that is either the serialised Class of the Throwable or empty
+     */
+    private byte[] getExceptionData(final Throwable t)
+    {
+        try {
+            return getCodec().serialise(t.getClass());
+        } catch (CodecException e) {
+            LOG.warn("Failed to serialise exception, continuing", e);
+            return new byte[]{};
+        }
+    }
+}
---
+++ src/main/java/com/hpe/caf/worker/AbstractWorkerFactory.java
@@ -0,0 +1,136 @@
+package com.hpe.caf.worker;
+
+
+import com.hpe.caf.api.Codec;
+import com.hpe.caf.api.CodecException;
+import com.hpe.caf.api.ConfigurationException;
+import com.hpe.caf.api.ConfigurationSource;
+import com.hpe.caf.api.DecodeMethod;
+import com.hpe.caf.api.worker.DataStore;
+import com.hpe.caf.api.worker.InvalidTaskException;
+import com.hpe.caf.api.worker.TaskRejectedException;
+import com.hpe.caf.api.worker.TaskStatus;
+import com.hpe.caf.api.worker.Worker;
+import com.hpe.caf.api.worker.WorkerException;
+import com.hpe.caf.api.worker.WorkerFactory;
+
+import java.util.Objects;
+
+
+/**
+ * A WorkerFactory that uses a pre-defined configuration and Worker task class.
+ * This is the recommended abstract base to create a WorkerFactory from.
+ *
+ * @param <C> the Worker Configuration type parameter
+ * @param <T> the Worker Task type parameter
+ * @since 9.0
+ */
+public abstract class AbstractWorkerFactory<C, T> implements WorkerFactory
+{
+    private final DataStore dataStore;
+    private final C configuration;
+    private final Codec codec;
+    private final Class<T> taskClass;
+
+
+    /**
+     * Instantiates a new DefaultWorkerFactory.
+     * @param configSource the worker configuration source
+     * @param dataStore the external data store
+     * @param codec the codec used in serialisation
+     * @param configurationClass the worker configuration class
+     * @param taskClass the worker task class
+     * @throws WorkerException if the factory cannot be instantiated
+     */
+    public AbstractWorkerFactory(final ConfigurationSource configSource, final DataStore dataStore, final Codec codec,
+                                 final Class<C> configurationClass, final Class<T> taskClass)
+        throws WorkerException
+    {
+        this.codec = Objects.requireNonNull(codec);
+        this.taskClass = Objects.requireNonNull(taskClass);
+        this.dataStore = Objects.requireNonNull(dataStore);
+        try {
+            this.configuration = configSource.getConfiguration(configurationClass);
+        } catch (ConfigurationException e) {
+            throw new WorkerException("Failed to create worker factory", e);
+        }
+    }
+
+
+    /**
+     * {@inheritDoc}
+     * Verify that the incoming task has the right type and is a version that can be handled.
+     */
+    @Override
+    public final Worker getWorker(final String classifier, final int version, final TaskStatus status, final byte[] data, final byte[] context)
+        throws TaskRejectedException, InvalidTaskException
+    {
+        // Reject tasks of the wrong type and tasks that require a newer version
+        final String workerName = getWorkerName();
+        if (!workerName.equals(classifier)) {
+            throw new InvalidTaskException("Task of type " + classifier + " found on queue for " + workerName);
+        }
+
+        final int workerApiVersion = getWorkerApiVersion();
+
+        if (workerApiVersion < version) {
+            throw new TaskRejectedException("Found task version " + version + ", which is newer than " + workerApiVersion);
+        }
+
+        try {
+             return createWorker(codec.deserialise(data, taskClass, DecodeMethod.LENIENT));
+        } catch (CodecException e) {
+            throw new InvalidTaskException("Invalid input message", e);
+        }
+    }
+
+
+    /**
+     * Returns the name of the worker to use when checking the task type
+     */
+    protected abstract String getWorkerName();
+
+
+    /**
+     * Returns the maximum version of the worker message that is supported
+     */
+    protected abstract int getWorkerApiVersion();
+
+
+    /**
+     * Create a Worker instance.
+     * @param task the deserialised Worker task
+     * @return the worker instance
+     * @throws TaskRejectedException if a Worker cannot be created to handle this task currently
+     * @throws InvalidTaskException if it appears this task cannot possibly be handled by a Worker of this type
+     */
+    protected abstract Worker createWorker(final T task)
+        throws TaskRejectedException, InvalidTaskException;
+
+
+    /**
+     * @return the data store supplied by the application
+     */
+    protected DataStore getDataStore()
+    {
+        return dataStore;
+    }
+
+
+    /**
+     * @return the configuration associated with this WorkerFactory
+     */
+    protected C getConfiguration()
+    {
+        return configuration;
+    }
+
+
+    /**
+     * @return the codec supplied by the application for de/serialisation purposes
+     */
+    protected Codec getCodec()
+    {
+        return codec;
+    }
+}
