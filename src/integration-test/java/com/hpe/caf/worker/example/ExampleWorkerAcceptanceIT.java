db0c036305e2aee78950cfcd88665a66dcd32a30
smitcona <conal.smith@hpe.com>
2016-01-27 18:19:45 +0000

    Removed testing dependencies and acceptance test.

--- src/integration-test/java/com/hpe/caf/worker/example/ExampleWorkerAcceptanceIT.java
+++ src/integration-test/java/com/hpe/caf/worker/example/ExampleWorkerAcceptanceIT.java
@@ -1,15 +1,15 @@
-package com.hpe.caf.worker.example;
-
-import com.hpe.caf.worker.testing.execution.TestRunner;
-import org.junit.Test;
-
-/**
- * Created by smitcona on 22/01/2016.
- */
-public class ExampleWorkerAcceptanceIT {
-
-    @Test
-    public void testWorker() throws Exception {
-        TestRunner.runTests(new ExampleTestControllerProvider());
-    }
-}
+//package com.hpe.caf.worker.example;
+//
+//import com.hpe.caf.worker.testing.execution.TestRunner;
+//import org.junit.Test;
+//
+///**
+// * Created by smitcona on 22/01/2016.
+// */
+//public class ExampleWorkerAcceptanceIT {
+//
+//    @Test
+//    public void testWorker() throws Exception {
+//        TestRunner.runTests(new ExampleTestControllerProvider());
+//    }
+//}
