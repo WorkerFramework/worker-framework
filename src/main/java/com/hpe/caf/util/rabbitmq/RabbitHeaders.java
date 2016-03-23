8b681867451f0b09efd7e887468f534f9096c9b2
Philip Crooks <philip.crooks@hpe.com>
2016-03-23 17:26:39 +0000

    CAF_600: Initial version of JobTrackingWorker. The JobTrackingWorkerReporter is still to be implemented, interfacing with the Job Database.

---
+++ src/main/java/com/hpe/caf/util/rabbitmq/RabbitHeaders.java
@@ -0,0 +1,11 @@
+package com.hpe.caf.util.rabbitmq;
+
+
+/**
+ * CAF RabbitMQ headers
+ */
+public class RabbitHeaders
+{
+    public static final String RABBIT_HEADER_CAF_WORKER_REJECTED = "x-caf-worker-rejected";
+    public static final String RABBIT_HEADER_CAF_WORKER_RETRY = "x-caf-worker-retry";
+}
