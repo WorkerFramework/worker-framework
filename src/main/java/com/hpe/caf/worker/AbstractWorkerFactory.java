efae56e622fdcc76f7c408e04e9f11e49da5baba
Philip Crooks <philip.crooks@hpe.com>
2016-04-01 17:05:30 +0100

    CAF-599: Making task tracking info available to workers that require it.

--- src/main/java/com/hpe/caf/worker/AbstractWorkerFactory.java
+++ src/main/java/com/hpe/caf/worker/AbstractWorkerFactory.java
@@ -6,13 +6,7 @@ import com.hpe.caf.api.CodecException;
 import com.hpe.caf.api.ConfigurationException;
 import com.hpe.caf.api.ConfigurationSource;
 import com.hpe.caf.api.DecodeMethod;
-import com.hpe.caf.api.worker.DataStore;
-import com.hpe.caf.api.worker.InvalidTaskException;
-import com.hpe.caf.api.worker.TaskRejectedException;
-import com.hpe.caf.api.worker.TaskStatus;
-import com.hpe.caf.api.worker.Worker;
-import com.hpe.caf.api.worker.WorkerException;
-import com.hpe.caf.api.worker.WorkerFactory;
+import com.hpe.caf.api.worker.*;
 
 import java.util.Objects;
 
@@ -62,13 +56,13 @@ public abstract class AbstractWorkerFactory<C, T> implements WorkerFactory
      * Verify that the incoming task has the right type and is a version that can be handled.
      */
     @Override
-    public final Worker getWorker(final String classifier, final int version, final TaskStatus status, final byte[] data, final byte[] context)
+    public final Worker getWorker(final String classifier, final int version, final TaskStatus status, final byte[] data, final byte[] context, TrackingInfo tracking)
         throws TaskRejectedException, InvalidTaskException
     {
         // Reject tasks of the wrong type and tasks that require a newer version
         final String workerName = getWorkerName();
         if (!workerName.equals(classifier)) {
-            return getMismatchedWorker(classifier, version, status, data, context);
+            return getMismatchedWorker(classifier, version, status, data, context, tracking);
         }
 
         final int workerApiVersion = getWorkerApiVersion();
@@ -78,14 +72,14 @@ public abstract class AbstractWorkerFactory<C, T> implements WorkerFactory
         }
 
         try {
-             return createWorker(codec.deserialise(data, taskClass, DecodeMethod.LENIENT));
+             return createWorker(codec.deserialise(data, taskClass, DecodeMethod.LENIENT), tracking);
         } catch (CodecException e) {
             throw new InvalidTaskException("Invalid input message", e);
         }
     }
 
 
-    public Worker getMismatchedWorker(final String classifier, final int version, final TaskStatus status, final byte[] data, final byte[] context) throws TaskRejectedException, InvalidTaskException {
+    public Worker getMismatchedWorker(final String classifier, final int version, final TaskStatus status, final byte[] data, final byte[] context, TrackingInfo tracking) throws TaskRejectedException, InvalidTaskException {
         throw new InvalidTaskException("Task of type " + classifier + " found on queue for " + getWorkerName());
     }
 
@@ -103,6 +97,20 @@ public abstract class AbstractWorkerFactory<C, T> implements WorkerFactory
 
 
     /**
+     * Create a Worker instance with access to the tracking info associated with the task.
+     * This method should be overridden by any Worker Factory creating Worker instances that need access to tracking info.
+     * @param task the deserialised Worker task
+     * @param tracking additional fields used in tracking task messages
+     * @return the worker instance
+     * @throws TaskRejectedException if a Worker cannot be created to handle this task currently
+     * @throws InvalidTaskException if it appears this task cannot possibly be handled by a Worker of this type
+     */
+    protected Worker createWorker(final T task, TrackingInfo tracking) throws TaskRejectedException, InvalidTaskException {
+        return createWorker(task);
+    }
+
+
+    /**
      * Create a Worker instance.
      * @param task the deserialised Worker task
      * @return the worker instance
@@ -110,7 +118,7 @@ public abstract class AbstractWorkerFactory<C, T> implements WorkerFactory
      * @throws InvalidTaskException if it appears this task cannot possibly be handled by a Worker of this type
      */
     protected abstract Worker createWorker(final T task)
-        throws TaskRejectedException, InvalidTaskException;
+            throws TaskRejectedException, InvalidTaskException;
 
 
     /**
