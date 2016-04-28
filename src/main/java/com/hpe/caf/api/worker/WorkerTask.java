207609d02ddf4ba93300380ddbb90a8d7e57ff44
Dermot Hardy <dermot.hardy@hpe.com>
2016-04-28 18:56:59 +0100

    CAF-987: Add interfaces to support bulk processing in workers

---
+++ src/main/java/com/hpe/caf/api/worker/WorkerTask.java
@@ -0,0 +1,52 @@
+package com.hpe.caf.api.worker;
+
+/**
+ * Represents a task to be completed by the CAF Worker.
+ */
+public interface WorkerTask {
+
+    /**
+     * Retrieves an indicator of the type of the task
+     */
+    String getClassifier();
+
+    /**
+     * Retrieves the version of the task message used
+     */
+    int getVersion();
+
+    /**
+     * Retrieves the task status
+     */
+    TaskStatus getStatus();
+
+    /**
+     * Retrieves the actual task data in a serialised form
+     */
+    byte[] getData();
+
+    /**
+     * Retrieves any task specific context associated with the task
+     */
+    byte[] getContext();
+
+    /**
+     * Retrieves tracking information associated with the task
+     */
+    TrackingInfo getTrackingInfo();
+
+    /**
+     * Used by the Worker to set the response to the task.
+     */
+    void setResponse(WorkerResponse response);
+
+    /**
+     * Used by the Worker to reject the task
+     */
+    void setResponse(TaskRejectedException taskRejectedException);
+
+    /**
+     * Used by the Worker to declare that the task is not valid
+     */
+    void setResponse(InvalidTaskException invalidTaskException);
+}
