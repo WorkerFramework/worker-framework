0643f0970ff6ada3f0dbe937b74e7f80f34c4a39
Adam Rogan <adam.pau.rogan@hpe.com>
2016-11-30 19:38:01 +0000

    CAF-1393: Fixed changes required after pull request. Also uncommented a line of code that had gotten caught up in the header

--- worker-archetype/src/main/resources/archetype-resources/__rootArtifactId__-container/worker.sh
+++ worker-archetype/src/main/resources/archetype-resources/__rootArtifactId__-container/worker.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#
 # (c) Copyright 2015-2016 Hewlett Packard Enterprise Development LP
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 dropwizardConfig="/maven/worker.yaml"
 
--- worker-example/worker-example-container/worker.sh
+++ worker-example/worker-example-container/worker.sh
@@ -10,7 +10,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# dropwizardConfig="/maven/worker.yaml"
+
+ dropwizardConfig="/maven/worker.yaml"
 
 ####################################################
 # Sets the dropwizard config file to a path passed in by environment variable if a variable was passed in and the file exists there.
