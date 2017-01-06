72f56dd86e6ac976b84b2a52d35e247750891343
Gregory Lucy <gregory.lucy@hpe.com>
2017-01-06 15:04:12 +0000

    Updated worker-example-container/pom.xml to use worker-framework version 1.2.0-SNAPSHOT.  Updated ExampleWorkerAcceptanceIT to use TestNG as the previous test setup in ExampleWorkerAcceptanceIT is no longer supported in worker-framework version 1.2.0-SNAPSHOT

--- worker-example/worker-example-container/src/test/java/com/hpe/caf/worker/example/ExampleWorkerAcceptanceIT.java
+++ worker-example/worker-example-container/src/test/java/com/hpe/caf/worker/example/ExampleWorkerAcceptanceIT.java
@@ -15,20 +15,49 @@
 
 package com.hpe.caf.worker.example;
 
-import com.hpe.caf.worker.testing.execution.TestRunner;
-import org.junit.Test;
+import com.hpe.caf.worker.testing.TestControllerSingle;
+import com.hpe.caf.worker.testing.TestItem;
+import com.hpe.caf.worker.testing.UseAsTestName;
+import com.hpe.caf.worker.testing.UseAsTestName_TestBase;
+import com.hpe.caf.worker.testing.execution.TestControllerProvider;
+import com.hpe.caf.worker.testing.execution.TestRunnerSingle;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * Integration test for Example worker, running the testing framework.
  */
-public class ExampleWorkerAcceptanceIT {
+public class ExampleWorkerAcceptanceIT extends UseAsTestName_TestBase {
 
-    /**
-     * Run integration tests using the testing framework.
-     * @throws Exception
-     */
-    @Test
-    public void testWorker() throws Exception {
-        TestRunner.runTests(new ExampleTestControllerProvider());
+    TestControllerProvider testControllerProvider;
+    TestControllerSingle controller;
+
+    @BeforeClass
+    public void setUp() throws Exception {
+        testControllerProvider = new ExampleTestControllerProvider();
+        controller =  TestRunnerSingle.getTestController(testControllerProvider, false);
+        controller.initialise();
+    }
+
+    @AfterClass
+    public void tearDown() throws Exception{
+        controller.close();
+    }
+
+    @DataProvider(name = "MainTest")
+    public Iterator<Object[]> createData() throws Exception {
+        Set<Object[]> s = TestRunnerSingle.setUpTest(testControllerProvider);
+        return s.iterator();
+    }
+
+    @UseAsTestName(idx = 1)
+    @Test(dataProvider = "MainTest")
+    public void testWorker(TestItem testItem, String testName) throws Exception {
+        controller.runTests(testItem);
     }
 }
