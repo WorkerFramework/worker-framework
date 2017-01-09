c183fb98057a296e1b7e33d0a9d9f62fc661ab1c
Gregory Lucy <gregory.lucy@hpe.com>
2017-01-09 17:01:37 +0000

    WorkerQueue cannot be null ever at this point.  WorkerQueue should not be null for either a Dead End Worker or an Ordinary Worker.  The comment was misplaced and confusing, therefore removed.  Updated variable name from queue to workerQueue to avoid confusion.

--- worker-core/src/main/java/com/hpe/caf/worker/core/WorkerCore.java
+++ worker-core/src/main/java/com/hpe/caf/worker/core/WorkerCore.java
@@ -355,10 +355,10 @@ final class WorkerCore
         private final WorkerStats stats;
 
 
-        public CoreWorkerCallback(final Codec codec, final WorkerQueue queue, final WorkerStats stats)
+        public CoreWorkerCallback(final Codec codec, final WorkerQueue workerQueue, final WorkerStats stats)
         {
             this.codec = Objects.requireNonNull(codec);
-            this.workerQueue = queue;  // workerQueue can be null for a dead end worker
+            this.workerQueue = Objects.requireNonNull(workerQueue);
             this.stats = Objects.requireNonNull(stats);
         }
 
