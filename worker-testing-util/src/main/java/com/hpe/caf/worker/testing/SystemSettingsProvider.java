5c302b05a2dff4a9eb73b90c6b10822f94ab0d79
Trevor Getty <trevor.getty@hpe.com>
2017-01-24 13:51:24 +0000

    Reverted SettingsProvider to get System and environment properties directly instead of using commons-configuration, which had issues incorrect interpolating "\\\\" chars.

--- worker-testing-util/src/main/java/com/hpe/caf/worker/testing/SystemSettingsProvider.java
+++ worker-testing-util/src/main/java/com/hpe/caf/worker/testing/SystemSettingsProvider.java
@@ -15,9 +15,6 @@
 
 package com.hpe.caf.worker.testing;
 
-import org.apache.commons.configuration.CompositeConfiguration;
-import org.apache.commons.configuration.EnvironmentConfiguration;
-import org.apache.commons.configuration.SystemConfiguration;
 
 import java.util.Objects;
 
@@ -26,33 +23,9 @@ import java.util.Objects;
  */
 public class SystemSettingsProvider extends SettingsProvider {
 
-    private final CompositeConfiguration configuration = createConfiguration();
-
-    private static CompositeConfiguration createConfiguration() {
-        CompositeConfiguration configuration = new CompositeConfiguration();
-        configuration.addConfiguration(new SystemConfiguration());
-        configuration.addConfiguration(new EnvironmentConfiguration());
-        return configuration;
-    }
-
-    public CompositeConfiguration getConfiguration() {
-        return configuration;
-    }
-
     @Override
     public String getSetting(String name) {
         Objects.requireNonNull(name);
-       // return System.getProperty(name, System.getenv(name));
-        return configuration.getString(name);
-    }
-
-    @Override
-    public boolean getBooleanSetting(String name) {
-        return configuration.getBoolean(name);
-    }
-
-    @Override
-    public boolean getBooleanSetting(String name, boolean defaultValue) {
-        return configuration.getBoolean(name, defaultValue);
+        return System.getProperty(name, System.getenv(name));
     }
 }
