2382de832ce79d6d6910c172a902b73b586e163d
Philip Crooks <philip.crooks@hpe.com>
2017-02-28 12:47:21 +0000

    CAF-2306: Extended and cleaned up tests.

--- worker-testing-integration/src/test/java/com/hpe/caf/worker/testing/validation/CustomPropertyValidatorTest.java
+++ worker-testing-integration/src/test/java/com/hpe/caf/worker/testing/validation/CustomPropertyValidatorTest.java
@@ -15,7 +15,6 @@
  */
 package com.hpe.caf.worker.testing.validation;
 
-import com.google.common.base.Strings;
 import com.hpe.caf.worker.testing.TestConfiguration;
 import com.hpe.caf.worker.testing.configuration.ValidationSettings;
 import org.testng.annotations.Test;
@@ -33,69 +32,141 @@ public class CustomPropertyValidatorTest {
      */
     @Test
     public void testCustomValidatorsUsed() {
+        final String INT_PROP_1 = "intProp1";
+        final String INT_PROP_2 = "intProp2";
+        final String STRING_PROP_1 = "stringProp1";
+
         final ValidationSettings validationSettings = ValidationSettings.configure()
-                .customValidators(new CustomIntByNamePropertyValidator("intProp1", "intProp2"), new CustomStringPropertyValidator())
+                .customValidators(new CustomIntPropertyValidator(INT_PROP_1, INT_PROP_2), new CustomIntPropertyValidator(), new CustomStringPropertyValidator())
                 .build();
 
         final ValidatorFactory validatorFactory = new ValidatorFactory(validationSettings, null, null, TestConfiguration.createDefault(null, null, null, null));
 
-        final PropertyValidator namedIntProp1Validator = validatorFactory.create("intProp1", 42, 42);
+        final PropertyValidator namedIntProp1Validator = validatorFactory.create(INT_PROP_1, 42, 42);
+        assertTrue(namedIntProp1Validator instanceof CustomPropertyValidator, "Expected a CustomPropertyValidator as we've configured a CustomIntPropertyValidator that accepts the name of the property being validated");
         assertTrue(namedIntProp1Validator.isValid(42, 42));
-        final PropertyValidator namedIntProp2Validator = validatorFactory.create("intProp2", 999, 999);
+
+        final PropertyValidator namedIntProp2Validator = validatorFactory.create(INT_PROP_2, 999, 999);
+        assertTrue(namedIntProp2Validator instanceof CustomPropertyValidator, "Expected a CustomPropertyValidator as we've configured a CustomIntPropertyValidator that accepts the name of the property being validated");
         assertTrue(namedIntProp2Validator.isValid(999, 999));
-        assertEquals(namedIntProp1Validator, namedIntProp2Validator, "Expected the same validator to be used for both unnamed int property validations");
+
+        assertEquals(namedIntProp1Validator, namedIntProp2Validator, "Expected the same validator instance to be used for both named int property validations as we've configured a CustomIntPropertyValidator that accepts the names of both properties being validated");
 
         final PropertyValidator unnamedIntPropValidator = validatorFactory.create(null, 42, 42);
+        assertTrue(unnamedIntPropValidator instanceof CustomPropertyValidator, "Expected a CustomPropertyValidator as we've configured a CustomIntPropertyValidator that ignores the name of the property being validated");
         assertTrue(unnamedIntPropValidator.isValid(42, 42));
-        assertNotEquals(namedIntProp1Validator, unnamedIntPropValidator, "Expected different validators to be used for named and unnamed int property validations");
 
-        final PropertyValidator stringProp1Validator = validatorFactory.create("stringProp1", "hello world", "hello world");
-        assertTrue(stringProp1Validator.isValid("hello world", "hello world"));
-        final PropertyValidator stringProp2Validator = validatorFactory.create(null, "go away", "go away");
-        assertTrue(stringProp2Validator.isValid("go away", "go away"));
-        assertEquals(stringProp1Validator, stringProp2Validator, "Expected the same validator to be used for named and unnamed string property validations");
+        assertNotEquals(namedIntProp1Validator, unnamedIntPropValidator, "Expected different validator instances for named and unnamed int property validations");
+
+        final PropertyValidator namedStringPropValidator = validatorFactory.create(STRING_PROP_1, "hello world", "hello world");
+        assertTrue(namedStringPropValidator instanceof CustomPropertyValidator, "Expected a CustomPropertyValidator as we've configured a CustomStringPropertyValidator that ignores the name of the property being validated");
+        assertTrue(namedStringPropValidator.isValid("hello world", "hello world"));
+
+        final PropertyValidator unnamedStringPropValidator = validatorFactory.create(null, "go away", "go away");
+        assertTrue(unnamedStringPropValidator instanceof CustomPropertyValidator, "Expected a CustomPropertyValidator as we've configured a CustomStringPropertyValidator that ignores the name of the property being validated");
+        assertTrue(unnamedStringPropValidator.isValid("go away", "go away"));
+
+        assertEquals(namedStringPropValidator, unnamedStringPropValidator, "Expected the same validator instance to be used for both named and unnamed string property validations as we've configured a CustomStringPropertyValidator that ignores the names of the property being validated");
     }
 
     @Test
-    public void testCustomValidatorNotUsed() {
+    public void testCustomValidatorCreatedWithDifferingPropertyValues() {
+        final String INT_PROP_1 = "intProp1";
+
         final ValidationSettings validationSettings = ValidationSettings.configure()
-                .customValidators(new CustomIntByNamePropertyValidator("intProp1", "intProp2"))
+                .customValidators(new CustomIntPropertyValidator(INT_PROP_1))
                 .build();
 
         final ValidatorFactory validatorFactory = new ValidatorFactory(validationSettings, null, null, TestConfiguration.createDefault(null, null, null, null));
 
-        final PropertyValidator validator = validatorFactory.create("intProp3", 42, 42);
+        final PropertyValidator validator = validatorFactory.create(INT_PROP_1, 42, 53);
+        assertTrue(validator instanceof CustomPropertyValidator, "Expected a CustomPropertyValidator for named int property validation despite specifying differing values when creating the validator, as we've configured a CustomIntPropertyValidator that accepts the name of the property being validated");
+        assertFalse(validator.isValid(42, 53));
+    }
+
+    @Test
+    public void testCustomValidatorCreatedWithDifferingUnnamedPropertyValues() {
+        final ValidationSettings validationSettings = ValidationSettings.configure()
+                .customValidators(new CustomIntPropertyValidator())
+                .build();
+
+        final ValidatorFactory validatorFactory = new ValidatorFactory(validationSettings, null, null, TestConfiguration.createDefault(null, null, null, null));
+
+        final PropertyValidator validator = validatorFactory.create(null, 42, 53);
+        assertTrue(validator instanceof CustomPropertyValidator, "Expected a CustomPropertyValidator for unnamed int property validation despite specifying differing values when creating the validator, as we've configured a CustomIntPropertyValidator that ignores the name of the property being validated");
+        assertFalse(validator.isValid(42, 53));
+    }
+
+    @Test
+    public void testCustomValidatorNotUsedAsPropNameNotRecognized() {
+        final String INT_PROP_1 = "intProp1";
+        final String INT_PROP_2 = "intProp2";
+        final String INT_PROP_3 = "intProp3";
+
+        final ValidationSettings validationSettings = ValidationSettings.configure()
+                .customValidators(new CustomIntPropertyValidator(INT_PROP_1, INT_PROP_2))
+                .build();
+
+        final ValidatorFactory validatorFactory = new ValidatorFactory(validationSettings, null, null, TestConfiguration.createDefault(null, null, null, null));
+
+        final PropertyValidator validator = validatorFactory.create(INT_PROP_3, 42, 42);
+        assertFalse(validator instanceof CustomPropertyValidator, "Did not expect a CustomPropertyValidator as the configured CustomPropertyValidator does not accept the named property being validated");
         assertTrue(validator.isValid(42, 42));
-        assertFalse(validator instanceof CustomPropertyValidator, "Did not expect validator to be a CustomPropertyValidator");
     }
 
     @Test
-    public void testValidationFailureForTypeIncompatibility() {
+    public void testValidationFailureForValidationTypeIncompatibleWithValidator() {
+        final String INT_PROP_1 = "intProp1";
+
         final ValidationSettings validationSettings = ValidationSettings.configure()
-                .customValidators(new CustomIntByNamePropertyValidator("intProp4"))
+                .customValidators(new CustomIntPropertyValidator(INT_PROP_1))
                 .build();
 
         final ValidatorFactory validatorFactory = new ValidatorFactory(validationSettings, null, null, TestConfiguration.createDefault(null, null, null, null));
 
-        final PropertyValidator validator = validatorFactory.create("intProp4", 42, 42);
-        assertTrue(validator instanceof CustomPropertyValidator, "Expected validator to be a CustomPropertyValidator");
-        assertFalse(validator.isValid("not-an-int", "not-an-int"), "Expected validation failure as validator is testing types for which it was not created to validate");
+        final PropertyValidator validator = validatorFactory.create(INT_PROP_1, 42, 42);
+        assertTrue(validator instanceof CustomPropertyValidator, "Expected a CustomPropertyValidator as we've configured a CustomIntPropertyValidator that accepts the name of the property being validated");
+        assertFalse(validator.isValid("not-an-int", "not-an-int"), "Expected validation failure as the validator is testing types that it was not created to validate");
+    }
+
+    @Test
+    public void testChooseFirstOfMultipleApplicableValidators() {
+        final String INT_PROP_1 = "intProp1";
+        final String INT_PROP_2 = "intProp2";
+        final String INT_PROP_3 = "intProp3";
+
+        final ValidationSettings validationSettings = ValidationSettings.configure()
+                .customValidators(new CustomIntPropertyValidator(INT_PROP_1, INT_PROP_2), new CustomIntPropertyValidator(INT_PROP_2, INT_PROP_3))
+                .build();
+
+        final ValidatorFactory validatorFactory = new ValidatorFactory(validationSettings, null, null, TestConfiguration.createDefault(null, null, null, null));
+
+        final PropertyValidator validator = validatorFactory.create(INT_PROP_2, 42, 42);
+        assertTrue(validator instanceof CustomPropertyValidator, "Expected a CustomPropertyValidator as we've configured a CustomIntPropertyValidator that accepts the name of the property being validated");
+        assertTrue(validator instanceof CustomIntPropertyValidator, "Expected the validator to be specifically a CustomIntPropertyValidator");
+
+        CustomIntPropertyValidator customIntValidator = (CustomIntPropertyValidator)validator;
+        assertEquals(customIntValidator.getRecognizedPropertyNames().size(), 2, "Expected to use the configured CustomIntPropertyValidator that recognizes 2 property names");
+        assertTrue(customIntValidator.getRecognizedPropertyNames().contains(INT_PROP_1) && customIntValidator.getRecognizedPropertyNames().contains(INT_PROP_2), "Expected to use the configured CustomIntPropertyValidator that recognizes the property names \"" + INT_PROP_1 + "\" and \"" + INT_PROP_2 + "\"");
+        assertTrue(validator.isValid(42, 42));
     }
 
 
-    private static class CustomIntByNamePropertyValidator extends CustomPropertyValidator {
+    private static class CustomIntPropertyValidator extends CustomPropertyValidator {
         private Collection<String> recognizedPropertyNames = new ArrayList<>();
 
-        public CustomIntByNamePropertyValidator(final String... recognizedPropertyNames) {
+        public CustomIntPropertyValidator(final String... recognizedPropertyNames) {
             this.recognizedPropertyNames = new ArrayList<>(Arrays.asList(recognizedPropertyNames));
+        }
+
+        public Collection<String> getRecognizedPropertyNames() {
+            return recognizedPropertyNames;
         }
 
         @Override
         public boolean canValidate(String propertyName, Object sourcePropertyValue, Object validatorPropertyValue) {
-            return !Strings.isNullOrEmpty(propertyName) &&
-                    recognizedPropertyNames.contains(propertyName) &&
-                    sourcePropertyValue instanceof Integer &&
-                    validatorPropertyValue instanceof Integer;
+            final boolean nameCheckPassed = recognizedPropertyNames.isEmpty() || recognizedPropertyNames.contains(propertyName);
+            return nameCheckPassed && sourcePropertyValue instanceof Integer && validatorPropertyValue instanceof Integer;
         }
 
         @Override
