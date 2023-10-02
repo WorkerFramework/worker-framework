d0dde16450633a3a77ad62860f2cc95c57782d68
Dermot Hardy <dhardy2@opentext.com>
2023-10-02 11:48:45 +0100

    US807003: Added a test worker and check that it starts

---
+++ worker-test/src/main/java/com/github/workerframework/testworker/TestWorker.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2015-2023 Open Text.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.workerframework.testworker;
+
+import com.hpe.caf.api.worker.InvalidTaskException;
+import com.hpe.caf.api.worker.TaskRejectedException;
+import com.hpe.caf.api.worker.TaskStatus;
+import com.hpe.caf.api.worker.Worker;
+import com.hpe.caf.api.worker.WorkerResponse;
+import com.hpe.caf.api.worker.WorkerTaskData;
+import java.nio.charset.StandardCharsets;
+
+final class TestWorker implements Worker
+{
+    private static final byte[] TEST_WORKER_RESULT = "TestWorkerResult".getBytes(StandardCharsets.UTF_8);
+
+    private final TestWorkerConfiguration config;
+
+    public TestWorker(final TestWorkerConfiguration config, final WorkerTaskData workerTask)
+        throws InvalidTaskException, TaskRejectedException
+    {
+        this.config = config;
+    }
+
+    @Override
+    public WorkerResponse doWork() throws InterruptedException, TaskRejectedException, InvalidTaskException
+    {
+        final String outputQueue = config.getOutputQueue();
+
+        return new WorkerResponse(
+            outputQueue,
+            TaskStatus.RESULT_SUCCESS,
+            TEST_WORKER_RESULT,
+            "TestWorkerResult",
+            1,
+            null);
+    }
+
+    @Override
+    public WorkerResponse getGeneralFailureResult(final Throwable t)
+    {
+        final String outputQueue = config.getOutputQueue();
+
+        return new WorkerResponse(
+            outputQueue,
+            TaskStatus.RESULT_SUCCESS,
+            t.toString().getBytes(StandardCharsets.UTF_8),
+            "TestWorkerFailureResult",
+            1,
+            null);
+    }
+
+    @Override
+    public int getWorkerApiVersion()
+    {
+        return 1;
+    }
+
+    @Override
+    public String getWorkerIdentifier()
+    {
+        return "TestWorker";
+    }
+}
---
+++ worker-test/src/main/java/com/github/workerframework/testworker/TestWorkerFactory.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2015-2023 Open Text.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.workerframework.testworker;
+
+import com.hpe.caf.api.Codec;
+import com.hpe.caf.api.ConfigurationException;
+import com.hpe.caf.api.ConfigurationSource;
+import com.hpe.caf.api.HealthResult;
+import com.hpe.caf.api.worker.DataStore;
+import com.hpe.caf.api.worker.InvalidTaskException;
+import com.hpe.caf.api.worker.TaskRejectedException;
+import com.hpe.caf.api.worker.Worker;
+import com.hpe.caf.api.worker.WorkerException;
+import com.hpe.caf.api.worker.WorkerFactory;
+import com.hpe.caf.api.worker.WorkerTaskData;
+
+final class TestWorkerFactory implements WorkerFactory
+{
+    private final TestWorkerConfiguration config;
+
+    public TestWorkerFactory(
+        final ConfigurationSource configSource,
+        final DataStore dataStore,
+        final Codec codec
+    ) throws WorkerException
+    {
+        this.config = getConfiguration(configSource);
+    }
+
+    private static TestWorkerConfiguration getConfiguration(final ConfigurationSource configSource)
+        throws WorkerException
+    {
+        try {
+            return configSource.getConfiguration(TestWorkerConfiguration.class);
+        } catch (final ConfigurationException ex) {
+            throw new WorkerException("Failed to construct TestWorkerConfiguration object", ex);
+        }
+    }
+
+    @Override
+    public String getInvalidTaskQueue()
+    {
+        return config.getOutputQueue();
+    }
+
+    @Override
+    public Worker getWorker(final WorkerTaskData workerTask) throws TaskRejectedException, InvalidTaskException
+    {
+        return new TestWorker(config, workerTask);
+    }
+
+    @Override
+    public int getWorkerThreads()
+    {
+        return config.getThreads();
+    }
+
+    @Override
+    public HealthResult healthCheck()
+    {
+        return HealthResult.RESULT_HEALTHY;
+    }
+}
