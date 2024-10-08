00cf115b854dfd8e7cda420aa13d074766237a47
Rory <rtorney@opentext.com>
2024-10-08 11:53:24 +0100

    US969005: Read secrets from props

--- worker-default-configs/config/cfg~caf~worker~RabbitConfiguration.js
+++ worker-default-configs/config/cfg~caf~worker~RabbitConfiguration.js
@@ -21,5 +21,5 @@
     rabbitHost: getenv("CAF_RABBITMQ_HOST") || "rabbitmq",
     rabbitPort: getenv("CAF_RABBITMQ_PORT") || 5672,
     rabbitUser: getenv("CAF_RABBITMQ_USERNAME") || "guest",
-    rabbitPassword: getenv("CAF_RABBITMQ_PASSWORD") || "guest"
+    rabbitPassword: getenvorprop("CAF_RABBITMQ_PASSWORD") || "guest"
 });
