a07687588b4a106fba3369c2181702a26523c19f
Jonathan McCoubrey <jmccoubrey@opentext.com>
2025-04-17 15:09:11 +0100

    Introduce env var to set TLS protocol version

--- worker-default-configs/config/cfg~caf~worker~RabbitConfiguration.js
+++ worker-default-configs/config/cfg~caf~worker~RabbitConfiguration.js
@@ -18,6 +18,7 @@
     maxBackoffInterval: getenv("CAF_RABBITMQ_MAX_BACKOFF_INTERVAL") || 15,
     maxAttempts: getenv("CAF_RABBITMQ_MAX_ATTEMPTS") || 3,
     rabbitProtocol: getenv("CAF_RABBITMQ_PROTOCOL") || "amqp",
+    rabbitTlsProtocolVersion: getenv("CAF_RABBITMQ_TLS_PROTOCOL_VERSION") || "TLSv1.2",
     rabbitHost: getenv("CAF_RABBITMQ_HOST") || "rabbitmq",
     rabbitPort: getenv("CAF_RABBITMQ_PORT") || 5672,
     rabbitUser: getenv("CAF_RABBITMQ_USERNAME") || "guest",
