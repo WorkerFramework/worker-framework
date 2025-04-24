2f4f4f445c7abf1a0c7c42747584347a9f3779ed
Jonny Mccoubrey <44871582+jonny-mccoubrey@users.noreply.github.com>
2025-04-24 11:13:18 +0100

    US1016047: Introduce configurable TLS protocol version env var (#215)

--- util-rabbitmq/src/main/java/com/github/workerframework/util/rabbitmq/RabbitUtil.java
+++ util-rabbitmq/src/main/java/com/github/workerframework/util/rabbitmq/RabbitUtil.java
@@ -115,7 +115,7 @@ public final class RabbitUtil
                 final TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(tmfAlgorithm);
                 trustManagerFactory.init((KeyStore) null);
 
-                final SSLContext context = SSLContext.getInstance("TLSv1.2");
+                final SSLContext context = SSLContext.getInstance(rc.getRabbitTlsProtocolVersion());
                 context.init(null, trustManagerFactory.getTrustManagers(), null);
 
                 factory.useSslProtocol(context);
