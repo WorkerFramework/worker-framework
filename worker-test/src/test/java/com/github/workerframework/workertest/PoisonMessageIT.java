fa6a00517277d010d1a674e94c8441bbaa9b7535
David Milligan <dmilligan@opentext.com>
2025-05-09 11:56:22 +0100

    Updated PoisonMessageIT

--- worker-test/src/test/java/com/github/workerframework/workertest/PoisonMessageIT.java
+++ worker-test/src/test/java/com/github/workerframework/workertest/PoisonMessageIT.java
@@ -99,8 +99,8 @@ public class PoisonMessageIT  extends TestWorkerTestBase{
             final TaskMessage decodedBody = codec.deserialise(consumedByteArrayOpt.get(), TaskMessage.class);
             final String taskData = new String(decodedBody.getTaskData(), StandardCharsets.UTF_8);
 
-            Assert.assertTrue(taskData.contains(POISON_ERROR_MESSAGE));
             Assert.assertTrue(taskData.contains(WORKER_FRIENDLY_NAME));
+            Assert.assertTrue(taskData.contains(POISON_ERROR_MESSAGE));
         }
     }
 }
