f5d9eb9ff8294901eb5e67fd3602a60a0557a666
David Milligan <dmilligan@opentext.com>
2025-05-09 12:20:54 +0100

    Ignore GetWorkerNameIT

--- worker-test/src/test/java/com/github/workerframework/workertest/GetWorkerNameIT.java
+++ worker-test/src/test/java/com/github/workerframework/workertest/GetWorkerNameIT.java
@@ -26,6 +26,7 @@ import com.github.workerframework.util.rabbitmq.RabbitHeaders;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.AMQP;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import org.testng.Assert;
 
@@ -34,6 +35,7 @@ import java.util.Date;
 import java.util.Map;
 import java.util.HashMap;
 
+@Ignore
 public class GetWorkerNameIT extends TestWorkerTestBase {
     private static final String POISON_ERROR_MESSAGE = "could not process the item.";
     private static final String WORKER_FRIENDLY_NAME = "TestWorker";
