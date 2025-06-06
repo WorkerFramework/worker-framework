c32901c96079cb2a13fb08112d31f7e4ac5ebbe9
Andy Reid <andrew.reid@microfocus.com>
2025-06-06 11:13:42 +0100

    Remove Objects.requireNonNull(taskData);

--- worker-api/src/main/java/com/github/workerframework/api/TaskMessage.java
+++ worker-api/src/main/java/com/github/workerframework/api/TaskMessage.java
@@ -128,7 +128,7 @@ public final class TaskMessage
         this.taskId = Objects.requireNonNull(taskId);
         this.taskClassifier = Objects.requireNonNull(taskClassifier);
         this.taskApiVersion = Objects.requireNonNull(taskApiVersion);
-        this.taskData = Objects.requireNonNull(taskData);
+        this.taskData = taskData;
         this.taskStatus = Objects.requireNonNull(taskStatus);
         this.context = Objects.requireNonNull(context);
         this.to = to;
